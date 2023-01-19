753d319277fbf99c6df9424ff7e0fc2f837fb04a
amit <amittnn1823@gmail.com>
2023-01-20 00:18:41 +0200

    cec

--- project/src/main/java/cli/AirportCLI.java
+++ project/src/main/java/cli/AirportCLI.java
@@ -18,7 +18,7 @@ import service.TravelerService;
 public class AirportCLI {
 	public static Scanner sc = new Scanner(System.in);  // Create a Scanner object
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stu
 		//show flight
 		//show traveler
 		//add traveler
