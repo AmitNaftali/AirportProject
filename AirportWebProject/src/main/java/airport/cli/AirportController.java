cb975c5b0109a06c27cee678a42ed831a9cf18ce
PBar24 <crhkv123@gmail.com>
2023-01-31 15:27:39 +0200

    add show Flights To Destination and actions mapping

--- AirportWebProject/src/main/java/airport/cli/AirportController.java
+++ AirportWebProject/src/main/java/airport/cli/AirportController.java
@@ -6,9 +6,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import airport.entities.Flight;
+import airport.entities.Traveler;
 import airport.service.TravelerService;
 
 @Controller
@@ -27,4 +30,52 @@ public class AirportController {
 		}
 		return ""; //show destenation flights
 	}
+	
+	@RequestMapping("")
+    public String showFlightsToDestination (HttpServletRequest request, Model model){
+
+
+        String flightName= request.getParameter("flightName");
+
+        try {
+            List<Flight> destenation = service.showFlightsToDestinations(flightName);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return ""; //show all flights to selected destination
+    }
+	
+	@RequestMapping("/actions")
+    public String showActions()
+    {
+        return "show-actions"; //show actions flights
+    }
+    // Process the form
+    @RequestMapping("/processAction")
+
+    public String processAction(@ModelAttribute("flight") Flight flight,@ModelAttribute("user") User user ,HttpServletRequest request,Model modle) {
+        int action = (int) request.getAttribute("action");
+        return "hello-student";
+    }
+
+    public String actions(int decision,TravelerService service,Flight dest,User user) throws Exception {
+        Traveler t = new Traveler(Integer.parseInt(user.getPassword()),user.getUsername());
+        switch(decision) {
+        case 1:
+            service.addTravelerToFlight(dest.getId(), t);
+            return"";//add page
+        case 2:
+            service.removeTravelerFromFlight(dest.getId(), t);
+            return"";//del page
+        case 3:
+            return "" ;//show traveler fligthts
+        case 4:
+            return "show-destinations";
+        case 0:
+            System.out.println("Exited.");
+            return "log_out";
+        }
+        return "";
+    }
 }
